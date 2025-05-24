8f50120fc10a3bc4912dbf8ddd836ba65518b917
JobinC <jobincbenny@gmail.com>
2025-05-24 14:41:39 +0530

    removed backend from frontend repository

--- backend/routes/checkout-route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const checkOutController = require('../controllers/checkout-controller')
-const verifyToken = require('../middlewares/tokenverification')
-
-router.post('/checkout',verifyToken,checkOutController.checkout);
-router.get('/get-checkout',verifyToken,checkOutController.getCheckout)
-module.exports = router;
--- backend/server.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const cors = require('cors');
-const modules = require('./app')
-modules.use(cors())
-const port = 3000;
-modules.listen(port,(req,res)=>{
-    console.log(`Server is listening on ${port}`)
-})
